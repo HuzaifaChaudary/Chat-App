185126c84f263b5e9956c41201f7926b4060957d
Huzaifa Iftikhar <98877942+HuzaifaChaudary@users.noreply.github.com>
2023-12-01 02:33:39 +0500

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,108 +1,91 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 import IntroScreens from '../saad/IntroScreens.js'
 import AppIntroSlider from "react-native-app-intro-slider"
-import { useFonts } from "expo-font";
-import { useCallback } from 'react';
+
 const Home = ({ navigation }) => {
-    const [fontsLoaded] = useFonts({
-        "Inter-Light": require("../../assets/fonts/Inter-Light.ttf"),
-        "Roboto-Flex": require("../../assets/fonts/Roboto-Medium.ttf"),
-        "Roboto-Bold": require("../../assets/fonts/Roboto-Bold.ttf"),
-      });
-    
-      const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded) {
-          await SplashScreen.hideAsync();
-        }
-      }, [fontsLoaded]);
-    
-      if (!fontsLoaded) {
-        return null;
-      }
-      const slides = [
+    const slides = [
         {
-          key: "slide1",
-          text: (
-            <Text
-              style={{
-                fontFamily: "Roboto-Bold",
-                fontSize: 28,
-                marginTop: 20,
-                fontWeight:700,
-                color: "white",
-                textAlign: "center",
-              }}
-            >
-              Im here for you {"\n"}{" "}
-              <Text style={{ color: "#C0A7D8" }}>During this tough time.</Text>
-            </Text>
-          ),
-          image: require("../../assets/Logo.svg"),
+            key: "slide1",
+            text: (
+                <Text
+                    style={{
+                        // fontFamily: "Roboto-Flex",
+                        fontSize: 25,
+                        marginTop: 20,
+                        color: "white",
+                        textAlign: "center",
+                    }}
+                >
+                    Im here for you {"\n"}{" "}
+                    <Text style={{ color: "#C0A7D8" }}>During this tough time.</Text>
+                </Text>
+            ),
+            image: require("../../assets/Logo.svg"),
         },
         {
-          key: "slide2",
-          text: (
-            <Text
-              style={{
-                fontFamily: "Roboto-Bold",
-                fontSize: 28,
-                marginTop: 20,
-                color: "white",
-                textAlign: "center",
-              }}
-            >
-              One of every <Text style={{ color: "#C0A7D8" }}>five</Text> people
-              experience a <Text style={{ color: "#C0A7D8" }}>loneliness.</Text>
-            </Text>
-          ),
-          image: require("../../assets/firstSliderScreen.png"),
+            key: "slide2",
+            text: (
+                <Text
+                    style={{
+                        fontFamily: "Roboto-Flex",
+                        fontSize: 25,
+                        marginTop: 20,
+                        color: "white",
+                        textAlign: "center",
+                    }}
+                >
+                    One of every <Text style={{ color: "#C0A7D8" }}>five</Text> people
+                    experience a <Text style={{ color: "#C0A7D8" }}>loneliness.</Text>
+                </Text>
+            ),
+            image: require("../../assets/firstSliderScreen.png"),
         },
         {
-          key: "slide3",
-          text: (
-            <Text
-              style={{
-                fontFamily: "Roboto-Bold",
-                fontSize: 28,
-                marginTop: 20,
-                color: "white",
-                textAlign: "center",
-              }}
-            >
-              <Text style={{ color: "#C0A7D8" }}>Stress</Text> has no {"\n"}{" "}
-              boundaries of age, {"\n"} gender, ethnicity, or {"\n"} religion
-            </Text>
-          ),
-          image: require("../../assets/SecondSliderScreen.png"),
+            key: "slide3",
+            text: (
+                <Text
+                    style={{
+                        fontFamily: "Roboto-Flex",
+                        fontSize: 25,
+                        marginTop: 20,
+                        color: "white",
+                        textAlign: "center",
+                    }}
+                >
+                    <Text style={{ color: "#C0A7D8" }}>Stress</Text> has no {"\n"}{" "}
+                    boundaries of age, {"\n"} gender, ethnicity, or {"\n"} religion
+                </Text>
+            ),
+            image: require("../../assets/SecondSliderScreen.png"),
         },
         {
-          key: "slide4",
-          text: (
-            <Text
-              style={{
-                fontFamily: "Roboto-Bold",
-                fontSize: 28,
-                marginTop: 20,
-                color: "white",
-                textAlign: "center",
-              }}
-            >
-              Stay <Text style={{ color: "#C0A7D8" }}>Happy</Text> and {"\n"}{" "}
-              surround yourself {"\n"} with{" "}
-              <Text style={{ color: "#C0A7D8" }}>Uplifting</Text> People.
-            </Text>
-          ),
-          image: require("../../assets/ThirdScreenSlider.png"),
+            key: "slide4",
+            text: (
+                <Text
+                    style={{
+                        fontFamily: "Roboto-Flex",
+                        fontSize: 25,
+                        marginTop: 20,
+                        color: "white",
+                        textAlign: "center",
+                    }}
+                >
+                    Stay <Text style={{ color: "#C0A7D8" }}>Happy</Text> and {"\n"}{" "}
+                    surround yourself {"\n"} with{" "}
+                    <Text style={{ color: "#C0A7D8" }}>Uplifting</Text> People.
+                </Text>
+            ),
+            image: require("../../assets/ThirdScreenSlider.png"),
         },
-      ];
+    ];
     return (
         <AppIntroSlider
             data={slides}
             renderItem={IntroScreens}
             showNextButton={false}
             onDone={() => {
-                navigation.navigate('NumberEnter')
+                navigation.navigate('Chat1')
             }}
             dotStyle={{
                 width: 10,
